f30b86971162e6bbaffd2b9638ba7c4534188132
akkos <a.kosheleva@g.nsu.ru>
2024-02-16 22:40:11 +0700

    add file

--- 1.3.py
+++ 1.3.py
@@ -1,8 +1,9 @@
-a = int(input())
-a = str(a)
-if len(a) > 7:
-    a = int(a)
-    b = a % 1000 // 100
+bitcoin = float(input())
+bitcoin = int(bitcoin)
+bitcoin = str(bitcoin)
+if len(bitcoin) == 7:
+    bitcoin = int(bitcoin)
+    b = bitcoin % 1000 // 100
     print(b)
 else:
     print('it is not a bitcoin')
